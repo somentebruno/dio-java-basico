4e5fcb09009de1b5c05d7d9b63872eefed48cc23
Bruno Lucas <brunolucasdev@gmail.com>
2024-05-13 14:46:38 -0400

    variaveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,11 @@
 public class MinhaClasse {
      public static void main (String [] args){
         System.out.print("Olá");
+
+        String meuNome = "Bruno";
+
+        int anoFabricacao = 2024;
+
+        boolean verdadeira = false; 
      }
 }
