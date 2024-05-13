0e8dd19a6564a53ca48fb3f161b0d85798dd7099
Bruno Lucas <brunolucasdev@gmail.com>
2024-05-13 14:52:38 -0400

    learning methodes

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,11 +1,16 @@
 public class MinhaClasse {
      public static void main (String [] args){
-        System.out.print("Olá");
+        String primeiroNome = "Bruno";
+        String segundoNome = "Lucas";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
 
-        String meuNome = "Bruno";
-
-        int anoFabricacao = 2024;
-
-        boolean verdadeira = false; 
      }
+
+     public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+     }
+
+
 }
