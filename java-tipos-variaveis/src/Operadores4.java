40175799c3d2340bce723118534c348ffd15b5f0
Bruno Lucas <brunolucasdev@gmail.com>
2024-05-14 10:26:19 -0400

    Variaveis de incremento

---
+++ java-tipos-variaveis/src/Operadores4.java
@@ -0,0 +1,13 @@
+public class Operadores4 {
+    public static void main(String[] args) {
+        //x repetições
+
+        int numero = 5;
+        numero++;
+
+
+        System.out.println(--numero);
+
+
+    }
+}
